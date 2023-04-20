b59c7e4952ed5c45e1fe26de9ff31b98d04dde82
Poylar <doaleks8@gmail.com>
2023-04-20 20:46:22 +0800

    deploy

--- src/assets/js/Cookies.js
+++ src/assets/js/Cookies.js
@@ -2,16 +2,19 @@ const element = document.querySelector('.cookies-banner');
 const acceptButton = document.querySelector('.cookies-banner__accept');
 const closeButton = document.querySelector('.cookies-banner__close');
 const value = localStorage.getItem('cookie-banner');
-if (!value) {
-  element.classList.add('active');
+
+if (element) {
+  if (!value) {
+    element.classList.add('active');
+  }
+
+
+  acceptButton.addEventListener('click', () => {
+    localStorage.setItem('cookie-banner', 'true');
+    element.remove();
+  })
+
+  closeButton.addEventListener('click', () => {
+    element.remove();
+  })
 }
-
-
-acceptButton.addEventListener('click', () => {
-  localStorage.setItem('cookie-banner', 'true');
-  element.remove();
-})
-
-closeButton.addEventListener('click', () => {
-  element.remove();
-})
