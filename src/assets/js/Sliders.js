49184a65d71ae8e075bdf7dec7bb5d9e08e3a6fa
Poylar <doaleks8@gmail.com>
2023-04-30 19:07:50 +0800

    deploy

--- src/assets/js/Sliders.js
+++ src/assets/js/Sliders.js
@@ -59,6 +59,35 @@ class Sliders {
       sliderInstance.init();
     });
   }
+
+  static defaultSlider() {
+    const roots = document.querySelectorAll('.js-default-slider');
+    roots.forEach((root) => {
+      const sliderInstance = new Swiper(root, {
+        modules: [Mousewheel],
+        init: false,
+
+        slidesPerView: 1.2,
+        spaceBetween: 20,
+
+        mousewheel: {
+          forceToAxis: true,
+          sensitivity: 0.3,
+        },
+
+        breakpoints: {
+          556: {
+            slidesPerView: 2.6,
+          },
+          768: {
+            slidesPerView: 3.6,
+          },
+        },
+      });
+
+      sliderInstance.init();
+    });
+  }
 }
 
 function slidersInit() {
