a1b6ce59e135e46ca10e2f3ca997065d8f71e90f
Poylar <doaleks8@gmail.com>
2023-05-01 15:10:30 +0800

    deploy

--- src/assets/js/Poper.js
+++ src/assets/js/Poper.js
@@ -1,46 +1,51 @@
 import { createPopper } from '@popperjs/core';
 
-const tooltipTrigger = document.querySelectorAll('[data-tooltip-trigger]');
-tooltipTrigger.forEach((trigger) => {
-  const tooltip = document.querySelector(`[data-tooltip="${trigger.dataset.tooltipTrigger}"]`);
-  const allTooltips = document.querySelectorAll('[data-tooltip]');
-  const popperInstance = createPopper(trigger, tooltip, {
-    placement: trigger.dataset.tooltipPlacement || 'right',
-    modifiers: [
-      {
-        name: 'offset',
-        options: {
-          offset: [0, 8],
+function popperInit() {
+  const tooltipTrigger = document.querySelectorAll('[data-tooltip-trigger]');
+  tooltipTrigger.forEach((trigger) => {
+    const tooltip = document.querySelector(`[data-tooltip="${trigger.dataset.tooltipTrigger}"]`);
+    const allTooltips = document.querySelectorAll('[data-tooltip]');
+    const popperInstance = createPopper(trigger, tooltip, {
+      placement: trigger.dataset.tooltipPlacement || 'right',
+      modifiers: [
+        {
+          name: 'offset',
+          options: {
+            offset: [0, 8],
+          },
         },
-      },
-    ],
-  });
+      ],
+    });
 
-  function hide() {
-    tooltip.removeAttribute('data-show');
-  }
-
-  // function show() {
-  //   tooltip.setAttribute('data-show', '');
-  //   popperInstance.update();
-  // }
-
-  function toggle() {
-    if (tooltip.dataset.show === '') {
+    function hide() {
       tooltip.removeAttribute('data-show');
-    } else {
-      allTooltips.forEach((t) => {
-        t.removeAttribute('data-show');
-      });
-      tooltip.setAttribute('data-show', '');
-      popperInstance.update();
     }
-  }
 
-  trigger.addEventListener('click', toggle);
-  document.addEventListener('click', (e) => {
-    if (!e.target.closest('.share')) {
-      hide();
+    // function show() {
+    //   tooltip.setAttribute('data-show', '');
+    //   popperInstance.update();
+    // }
+
+    function toggle() {
+      if (tooltip.dataset.show === '') {
+        tooltip.removeAttribute('data-show');
+      } else {
+        allTooltips.forEach((t) => {
+          t.removeAttribute('data-show');
+        });
+        tooltip.setAttribute('data-show', '');
+        popperInstance.update();
+      }
     }
+
+    trigger.addEventListener('click', toggle);
+    document.addEventListener('click', (e) => {
+      if (!e.target.closest('.share')) {
+        hide();
+      }
+    });
   });
-});
+
+}
+
+popperInit()
